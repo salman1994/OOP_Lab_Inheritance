e703788c4d68ee768707441c377957d3fee9aba9
salman1994 <salmanasim2012@gmail.com>
2016-05-21 14:22:33 +0500

    Task1
    Symphony Class Added

--- Task1.cpp
+++ Task1.cpp
@@ -21,7 +21,6 @@ class Band
 		{
 			this->name=name;
 		}
-		
 };
 
 class MetalBand: public Band
@@ -30,28 +29,27 @@ class MetalBand: public Band
 		int mem;
 		int tech;
 	
-	public:
+	public:	
+		void setMember(int mem)
+		{
+			this -> mem = mem;
+		}
 		
-	void setMember(int mem)
-	{
-		this -> mem = mem;
-	}
-	
-	void setTech(int tech)
-	{
-		this -> tech = tech;
-	}
-	
-	int getTech()
-	{
-		return this -> tech;
-	}
-	
-	int getcount()
-	{
-		count = mem + tech;
-		return count;
-	}
+		void setTech(int tech)
+		{
+			this -> tech = tech;
+		}
+		
+		int getTech()
+		{
+			return this -> tech;
+		}
+		
+		int getcount()
+		{
+			count = mem + tech;
+			return count;
+		}
 };
 
 class JazzBand: public Band
@@ -59,18 +57,17 @@ class JazzBand: public Band
 	private:
 		int mem;
 	
-	public:
+	public:	
+		void setMember(int mem)
+		{
+			this -> mem = mem;
+		}
 		
-	void setMember(int mem)
-	{
-		this -> mem = mem;
-	}
-	
-	int getcount()
-	{
-		count = mem;
-		return count;
-	}
+		int getcount()
+		{
+			count = mem;
+			return count;
+		}
 };
 
 class MarchingBand: public Band
@@ -79,19 +76,45 @@ class MarchingBand: public Band
 		int mem;
 	
 	public:
+		void setMember(int mem)
+		{
+			this -> mem = mem;
+		}
 		
-	void setMember(int mem)
-	{
-		this -> mem = mem;
-	}
-	
-	int getcount()
-	{
-		count = mem;
-		return count;
-	}
+		int getcount()
+		{
+			count = mem;
+			return count;
+		}
 };
 
+class Symphony: public Band
+{
+	private:
+		int mem;
+		int con;
+	
+	public:
+		Symphony()
+		{
+			con = 1;
+		}
+		void setMember(int mem)
+		{
+			this -> mem = mem;
+		}
+		
+		int getTech()
+		{
+			return this -> con;
+		}
+		
+		int getcount()
+		{
+			count = mem + con;
+			return count;
+		}
+};
 
 int main()
  {
@@ -113,5 +136,11 @@ int main()
 	K.setname("Marching Band");
  	cout << "Band Name: " << K.getname() << endl;
  	cout << "Total Members: " << K.getcount() << endl << endl;
+ 	
+ 	Symphony S;
+ 	S.setMember(9);
+	S.setname("Symphony");
+ 	cout << "Band Name: " << S.getname() << endl;
+ 	cout << "Total Members: " << S.getcount() << endl << endl;
  }
 
